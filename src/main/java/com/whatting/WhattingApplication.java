da10132580ef68e25939efb8f5aa83f76aefc56b
권수현 <iokshoi0915@dsm.hs.kr>
2026-07-13 15:37:04 +0900

    initial commit

---
+++ src/main/java/com/whatting/WhattingApplication.java
@@ -0,0 +1,13 @@
+package com.whatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhattingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whatting/WhattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
